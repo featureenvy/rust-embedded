598e85b70d5d322913c545bc0c791325e24d35d3
Andreas Arnold <andreas@featureenvy.com>
2016-03-08 19:54:59 +0100

    Finish implementing the state machine for lab 10.

--- src/labs/lab10.rs
+++ src/labs/lab10.rs
@@ -98,10 +98,18 @@ mod signaling_state {
     pub struct AllRed;
     pub struct GoWest;
     pub struct LastCallWest;
+    pub struct GoNorth;
+    pub struct LastCallNorth;
+    pub struct GoPedestrian;
+    pub struct LastCallPedestrian;
 
     pub static ALL_RED_REF: AllRed = AllRed;
     pub static GO_WEST_REF: GoWest = GoWest;
     pub static LAST_CALL_WEST_REF: LastCallWest = LastCallWest;
+    pub static GO_NORTH_REF: GoNorth = GoNorth;
+    pub static LAST_CALL_NORTH_REF: LastCallNorth = LastCallNorth;
+    pub static GO_PEDESTRIAN_REF: GoPedestrian = GoPedestrian;
+    pub static LAST_CALL_PEDESTRIAN_REF: LastCallPedestrian = LastCallPedestrian;
 }
 
 trait State {
@@ -111,10 +119,12 @@ trait State {
 }
 
 impl State for signaling_state::AllRed {
-    fn next(&self, _: bool, west: bool, _: bool) -> &'static State {
-        match west {
-            true => &signaling_state::GO_WEST_REF,
-            false => &signaling_state::ALL_RED_REF
+    fn next(&self, pedestrian: bool, west: bool, north: bool) -> &'static State {
+        match (pedestrian, west, north) {
+            (true, _, _) => &signaling_state::GO_PEDESTRIAN_REF,
+            (_, true, _) => &signaling_state::GO_WEST_REF,
+            (_, _, true) => &signaling_state::GO_NORTH_REF,
+            (_, _, _) => &signaling_state::ALL_RED_REF
         }
     }
 
@@ -150,8 +160,12 @@ impl State for signaling_state::GoWest {
 }
 
 impl State for signaling_state::LastCallWest {
-    fn next(&self, _: bool, _: bool, _: bool) -> &'static State {
-        &signaling_state::ALL_RED_REF
+    fn next(&self, pedestrian: bool, _: bool, north: bool) -> &'static State {
+        match (pedestrian, north) {
+            (_, true) => &signaling_state::GO_NORTH_REF,
+            (true, _) => &signaling_state::GO_PEDESTRIAN_REF,
+            (_, _) => &signaling_state::ALL_RED_REF
+        }
     }
 
     fn get_signal_values(&self) -> SignalValues {
@@ -167,11 +181,91 @@ impl State for signaling_state::LastCallWest {
     }
 }
 
+impl State for signaling_state::GoNorth {
+    fn next(&self, _: bool, _: bool, _: bool) -> &'static State {
+        &signaling_state::LAST_CALL_NORTH_REF
+    }
+
+    fn get_signal_values(&self) -> SignalValues {
+        SignalValues {
+            pedestrian: PedestrianColors::Red,
+            west: SignalColors::Red,
+            north: SignalColors::Green
+        }
+    }
+
+    fn wait_duration(&self) -> u64 {
+        50
+    }
+}
+
+impl State for signaling_state::LastCallNorth {
+    fn next(&self, pedestrian: bool, west: bool, _: bool) -> &'static State {
+        match (pedestrian, west) {
+            (true, _) => &signaling_state::GO_PEDESTRIAN_REF,
+            (_, true) => &signaling_state::GO_WEST_REF,
+            (_, _) => &signaling_state::ALL_RED_REF
+        }
+    }
+
+    fn get_signal_values(&self) -> SignalValues {
+        SignalValues {
+            pedestrian: PedestrianColors::Red,
+            west: SignalColors::Red,
+            north: SignalColors::Yellow
+        }
+    }
+
+    fn wait_duration(&self) -> u64 {
+        50
+    }
+}
+
+impl State for signaling_state::GoPedestrian {
+    fn next(&self, _: bool, _: bool, _: bool) -> &'static State {
+        &signaling_state::LAST_CALL_PEDESTRIAN_REF
+    }
+
+    fn get_signal_values(&self) -> SignalValues {
+        SignalValues {
+            pedestrian: PedestrianColors::Green,
+            west: SignalColors::Red,
+            north: SignalColors::Red
+        }
+    }
+
+    fn wait_duration(&self) -> u64 {
+        50
+    }
+}
+
+impl State for signaling_state::LastCallPedestrian {
+    fn next(&self, _: bool, west: bool, north: bool) -> &'static State {
+        match (west, north) {
+            (true, _) => &signaling_state::GO_WEST_REF,
+            (_, true) => &signaling_state::GO_NORTH_REF,
+            (_, _) => &signaling_state::ALL_RED_REF
+        }
+    }
+
+    fn get_signal_values(&self) -> SignalValues {
+        SignalValues {
+            pedestrian: PedestrianColors::Off,
+            west: SignalColors::Red,
+            north: SignalColors::Red
+        }
+    }
+
+    fn wait_duration(&self) -> u64 {
+        50
+    }
+}
+
 pub fn run() {
     clock::init();
 
     let pedestrian_red = led::Led::new(gpio::Port::PortF, gpio::Pins::Pin1);
-    let pedestrian_green = led::Led::new(gpio::Port::PortF, gpio::Pins::Pin0);
+    let pedestrian_green = led::Led::new(gpio::Port::PortF, gpio::Pins::Pin3);
     let pedestrian_waiting = switch::Switch::new(gpio::Port::PortE, gpio::Pins::Pin2, gpio::Logic::Negative);
 
     let pedestrian_signal = PedestrianSignal::new(pedestrian_red, pedestrian_green);
@@ -190,7 +284,6 @@ pub fn run() {
 
     let north_signal = Signal::new(north_red, north_yellow, north_green);
 
-    // let mut signaling_system = SignalingSystem::new();
     let mut signaling_system: &'static State = &signaling_state::ALL_RED_REF;
 
     loop {
